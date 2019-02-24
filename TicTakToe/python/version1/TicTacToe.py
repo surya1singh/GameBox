fef2cccdb4ec083c4f9a8fa17057281a6149cee5
surya pratap <surya1.singh@gmail.com>
2019-02-24 11:24:44 +0530

    add blueprint of flow

---
+++ TicTakToe/python/version1/TicTacToe.py
@@ -0,0 +1,24 @@
+
+
+
+class Board():
+    def __init__(self):
+        pass
+
+    def get_position(self, row, col):
+        pass
+
+    def set_position(self, row, col):
+        pass
+
+    def print_the_board(self):
+        pass
+
+    def print_the_board_for_input(self):
+        pass
+
+    def print_user_prompt(self):
+        pass
+
+    def check_win(self, char):
+        pass
---
+++ TicTakToe/python/version1/main.py
@@ -0,0 +1,13 @@
+from TicTacToe import Board
+
+
+def main():
+    # set user user name
+    # set board
+
+    # ask for user1 for input position
+    # check if user wins or not
+    # ask for user2 for input position
+    # check if user wins or not
+    # repeat this loop until any player wins or there is now input left to fill
+    
